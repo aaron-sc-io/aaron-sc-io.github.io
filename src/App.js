37ea4a7fc0742cce6bae8635d847b795dcfbc10d
Aaron <aaron.sc.taylor@gmail.com>
2024-04-25 07:33:11 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Body from "./Components/Body/Body";
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Body/Body.jsx
@@ -0,0 +1,13 @@
+import './Body.css';
+
+const Body = () => {
+  return (
+    <body>
+      <h2 class='topcorner'> 「 </h2>
+      <h1>AERYN ROBERTSON</h1>
+      <h2 className='bottomcorner'> 」 </h2>
+    </body>
+  )
+}
+
+export default Body;
