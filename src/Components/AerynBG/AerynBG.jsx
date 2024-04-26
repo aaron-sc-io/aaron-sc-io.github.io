a4580af78ce74ab1249c6ac540012d8117129881
Aaron <aaron.sc.taylor@gmail.com>
2024-04-26 09:34:47 -0400

    Main Body Fix

---
+++ src/Components/AerynBG/AerynBG.jsx
@@ -0,0 +1,14 @@
+import './AerynBG.css';
+import { Box } from '@mui/material';
+
+const AerynBG = () => {
+  return (
+    <Box sx={{ pl: '0.5em', pt: '20em' }}>
+      <h2 class='topcorner'> 「 </h2>
+      <h1>AERYN ROBERTSON</h1>
+      <h2 className='bottomcorner'> 」 </h2>
+    </Box>
+  )
+}
+
+export default AerynBG;
