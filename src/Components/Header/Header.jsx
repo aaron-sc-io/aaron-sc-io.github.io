c4e3c41b5bc3d3d929d96a3014c00638a0e24c93
Aaron <aaron.sc.taylor@gmail.com>
2024-04-28 13:54:25 -0400

    Header Mapping

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,31 +1,17 @@
-//import PropTypes from 'prop-types';
-//import './Header.css'
-import { Box } from '@mui/material';
 import NavItem from './NavItem/NavItem';
 
 const Header = () => {
-  
   let HeaderItems = [
-    {title: 'About', link: 'test'},
-    {title: 'Projects', link: 'test'},
-    {title: 'Contact', link: 'test'}
+    {title: 'About', link: 'About'},
+    {title: 'Projects', link: 'Projects'},
+    {title: 'Contact', link: 'Contact'}
   ];
   
   return (
-    <Box sx={{ flexDirection: 'row' }}>
-      {/* map? */}
-      {HeaderItems.forEach(item => {
-        <NavItem title={item.title} linkRef={item.link} />
-      })};
-
-      <NavItem title={HeaderItems[0].title} linkRef={HeaderItems[0].linkRef} />
-      <NavItem title={HeaderItems[1].title} linkRef={HeaderItems[1].linkRef} />
-      <NavItem title={HeaderItems[2].title} linkRef={HeaderItems[2].linkRef} />
-    </Box>
-
+      <>
+        {HeaderItems.map(item => {return (<NavItem title={item.title} linkRef={item.link} />)})}
+      </>
   )
-}
-
-//Header.propTypes = {}
+};
 
 export default Header;
